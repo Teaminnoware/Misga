848b6f18be6bb104409e273d26c8a3e68328a10d
Teaminnoware <stwijasinghe@gmail.com>
2018-10-08 19:05:03 +0530

    first try

---
+++ input.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en-US"><html>
+<head></head>
+<body style="background-color:green;">
+<h3 style="font-size:60px;">Matrix</h1>
+<p><h1 style="color:blue;">SIGN IN </P>
+<p>Email<input type="text" name="email"/> </p>
+<p>Password<input type="text" name="pass"/></p>
+<p><input type="Login" name="log" value="login"/></p>
+</form>
+</body>
+</html>
